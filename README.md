c236f819d6392435d91db851325215775ee3adeb
unknown <caionsantos2000@gmail.com>
2020-02-29 16:41:48 -0300

    add css

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # calcularMediaFBUNI
 
 https://caions.github.io/calcularMediaFBUNI/
+
+## ´Recursos utilizados´
+
+Css 
+Position absolute
+Bacground Gradient
+
+
--- main.js
+++ main.js
@@ -4,22 +4,30 @@ let submit = document.getElementById('submit');
 let texto = document.getElementById('texto');
 let texto2 = document.getElementById('texto2');
 
-let calcula = function(e){
-    let a = (parseFloat(v1.value) * 0.4 + parseFloat(v2.value) * 0.6);
-	texto.innerHTML = 'Sua media é = '+ a;
-	e.preventDefault();
-	status();
+let calcula = function (e) {
+    e.preventDefault();
+    if (v1.value > 10 || v2.value > 10) {
+        return texto.innerHTML = "Nota não pode ser superior a 10"
+    } else if (v1.value < 0 || v2.value < 0) {
+        return texto.innerHTML = "Nota não pode ser inferior a 0"
+    }
+    else {
+        let a = (parseFloat(v1.value) * 0.4 + parseFloat(v2.value) * 0.6);
+        texto.innerHTML = 'Sua média é : ' + a;
+        status();
+    }
+
 }
 
-let status = function(){
+let status = function () {
     let a = (parseFloat(v1.value) * 0.4 + parseFloat(v2.value) * 0.6);
-    if(a>=7){
-    return texto2.innerHTML = "Aprovado";
-	}
-	else if(a<4){
-	return texto2.innerHTML = "Reprovado sem direito a VS";
+    if (a >= 7) {
+        return texto2.innerHTML = "Aprovado!";
     }
-    else{
+    else if (a < 4) {
+        return texto2.innerHTML = "Reprovado sem direito a VS";
+    }
+    else {
         return texto2.innerHTML = "Está de VS";
     }
 }
